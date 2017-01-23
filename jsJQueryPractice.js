5170daa7ec51c1cbd112563b5948543106fc2c9e
George Higbie <gehigbie@gmail.com>
2017-01-23 00:05:24 -0700

    added important comments/notes

--- jsJQueryPractice.js
+++ jsJQueryPractice.js
@@ -1,1 +1,6 @@
 //created in the command line (for the second time)
+//$ = works just like document.querySelectorAll - it returns array of matching elements
+//$("img") - to select all img tags
+//$(".sale") - to select all elements with a class of sale
+//$("#bonus") - to select all elements with id of bonus
+//$("li a") - to select all a tags inside of li
