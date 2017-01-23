255fe8856ee3639a4b4064698b182a0970551b47
George Higbie <gehigbie@gmail.com>
2017-01-23 16:40:09 -0700

    removed bad file

--- jsCommanMethods.js
+++
@@ -1,6 +0,0 @@
-//created in command line
-//.text() can be used to set or get text , it strips out the html elements and only gives the text
-$("h1").text();
-$("ul").text();
-$("li").text(); //returns one string of all the li
-//if we pass in a value it changes the text
