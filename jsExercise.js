b73365c4d41f12e4ebb42f1b97f52eca095ab5df
George Higbie <gehigbie@gmail.com>
2017-01-23 14:41:04 -0700

    add jquery selectors

--- jsExercise.js
+++ jsExercise.js
@@ -1,1 +1,6 @@
 //created in command line
+
+$("div").css("background", "#7631b7");
+$(".highlight").css("width", "200px");
+$("#third").css("border",  "solid 1px #cc6d24");
+$("div").first().css("color", "#cc24bb");
