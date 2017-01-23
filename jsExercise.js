13aabc7200931076ab845dd65747cf2b6f4ab7c6
George Higbie <gehigbie@gmail.com>
2017-01-23 14:56:15 -0700

    added css selector

--- jsExercise.js
+++ jsExercise.js
@@ -9,3 +9,5 @@ $("div").css("background", "#7631b7");
 $(".highlight").css("width", "200px");
 $("#third").css("border",  "solid 1px #cc6d24");
 $("div").first().css("color", "#cc24bb");
+$("div:first-of-type").css("color", "#cc24bb"); //uses css selector
+$("div:first").css("color", "#cc24bb"); //this is built into jQuery and is a little slower
