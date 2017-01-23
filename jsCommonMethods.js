f111aab128d888e47b81ffe69e8881c647063a41
George Higbie <gehigbie@gmail.com>
2017-01-23 16:59:17 -0700

    add jQuery to js file

--- jsCommonMethods.js
+++ jsCommonMethods.js
@@ -4,3 +4,10 @@ $("h1").text();
 $("ul").text();
 $("li").text(); //returns one string of all the li
 //if we pass in a value it changes the text
+$("h1").text("The most awesome and the bestest jQuery Methods Page!!!");
+$("li").text("My cogi is is awesome!"); //this would require a loop in js
+
+//html() - gets the html contents of the first element in the set of matched elements or set the html content of evey mathcched element
+$("h1").html(); //this is the same as inner html
+$("ul").html("<li>I hacked your UL</li><li>My corgi is more awesome than yours</li>");
+$("li").html("<a href='http:\/\/www.google.com'>CLICK ME TO GO TO GOOGLE</a>");
