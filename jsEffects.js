28f2c0d6c54d0e7bad2a6e7fa743e7ceed370172
George Higbie <gehigbie@gmail.com>
2017-01-24 14:56:03 -0700

    changed page title and DOM and added js to remove elements

--- jsEffects.js
+++ jsEffects.js
@@ -1,2 +1,12 @@
 //this file was created in the command line
-$("div").fadeOut();
+//$("div").fadeOut();
+
+$("button").on("click", function(){
+	$("div").fadeOut(1500, function(){
+		console.log("Fade Completeed");
+		$(this).remove(function(){
+			console.log("divs removed from DOM");
+		})
+	});
+});
+//the divs are still in the DOM, but they are hdinging
