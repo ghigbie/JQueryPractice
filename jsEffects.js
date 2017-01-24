0f864fcb0c761c32b2a88602c192594f3fbd6780
George Higbie <gehigbie@gmail.com>
2017-01-24 15:10:32 -0700

    added fadeToggle method

--- jsEffects.js
+++ jsEffects.js
@@ -1,12 +1,28 @@
 //this file was created in the command line
 //$("div").fadeOut();
 
-$("button").on("click", function(){
-	$("div").fadeOut(1500, function(){
+// $("button").on("click", function(){
+// 	$("div").fadeOut(1500, function(){
+// 		console.log("Fade Completeed");
+// 		$(this).remove();
+// 		console.log("items removed");
+// 	});
+// });
+//the divs are still in the DOM, but they are hdinging
+
+// $("button").on("dblclick", function(){
+// 	$("p").fadeIn(2000, function(){
+// 		console.log("This paragraph has been brought into existence.");
+// 	});
+// });
+
+$("button").on("dblclick", function(){
+	$("p").fadeToggle(2000, function(){
+		console.log("This paragraph has been brought into existence.");
+	});
+	$("div").faddToggle(1500, function(){
 		console.log("Fade Completeed");
-		$(this).remove(function(){
-			console.log("divs removed from DOM");
-		})
+		$(this).remove();
+		console.log("items removed");
 	});
 });
-//the divs are still in the DOM, but they are hdinging
